59130a8d867d9bdf01d32296c5b5219ab2ecf1d6
Jeremy Rimpo <jeremy.rimpo@servermonkey.com>
2023-09-05 00:09:37 -0500

    Update translation files

--- src/game_starfield_en.ts
+++ src/game_starfield_en.ts
@@ -4,12 +4,12 @@
 <context>
     <name>GameStarfield</name>
     <message>
-        <location filename="gamestarfield.cpp" line="125"/>
+        <location filename="gamestarfield.cpp" line="127"/>
         <source>Starfield Support Plugin</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gamestarfield.cpp" line="136"/>
+        <location filename="gamestarfield.cpp" line="138"/>
         <source>Adds support for the game Starfield.</source>
         <translation type="unfinished"></translation>
     </message>
