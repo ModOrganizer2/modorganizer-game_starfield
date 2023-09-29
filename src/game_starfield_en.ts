52734be9bf4fabad848291c8d6f538661ba8de09
Jeremy Rimpo <jrim@rimpo.org>
2023-09-28 22:15:56 -0500

    Translation updates

--- src/game_starfield_en.ts
+++ src/game_starfield_en.ts
@@ -4,13 +4,19 @@
 <context>
     <name>GameStarfield</name>
     <message>
-        <location filename="gamestarfield.cpp" line="135"/>
+        <location filename="gamestarfield.cpp" line="107"/>
         <source>Starfield Support Plugin</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gamestarfield.cpp" line="145"/>
+        <location filename="gamestarfield.cpp" line="117"/>
         <source>Adds support for the game Starfield.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gamestarfield.cpp" line="129"/>
+        <source>Turn on plugin management. As of Starfield 1.7.33 this REQUIRES fixing &apos;plugins.txt&apos; with a SFSE plugin. This will do nothing otherwise.</source>
+        <oldsource>Turn on plugin management. As of Starfield 1.7.33 this REQUIRES SPECIAL WORKAROUNDS.</oldsource>
         <translation type="unfinished"></translation>
     </message>
 </context>
