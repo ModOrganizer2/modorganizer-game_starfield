1b9ca690f66afab1f37524008284b06136ad6639
Jeremy Rimpo <jrim@rimpo.org>
2023-10-08 15:53:37 -0500

    Add geometries to valid data and content type

--- src/starfieldmoddatacontent.h
+++ src/starfieldmoddatacontent.h
@@ -9,21 +9,27 @@ class StarfieldModDataContent : public GamebryoModDataContent
 protected:
   enum StarfieldContent
   {
-    CONTENT_MATERIAL = CONTENT_NEXT_VALUE
+    CONTENT_MATERIAL   = CONTENT_NEXT_VALUE,
+    CONTENT_GEOMETRIES = CONTENT_NEXT_VALUE + 1
   };
 
 public:
   StarfieldModDataContent(GameGamebryo const* gamePlugin)
       : GamebryoModDataContent(gamePlugin)
   {
-    m_Enabled[CONTENT_SKYPROC] = false;
+    m_Enabled[CONTENT_SKYPROC]    = false;
+    m_Enabled[CONTENT_MATERIAL]   = true;
+    m_Enabled[CONTENT_GEOMETRIES] = true;
   }
 
   std::vector<Content> getAllContents() const override
   {
+    static std::vector<Content> STARFIELD_CONTENTS{
+        {CONTENT_MATERIAL, QT_TR_NOOP("Materials"), ":/MO/gui/content/material"},
+        {CONTENT_GEOMETRIES, QT_TR_NOOP("Geometries"), ":/MO/gui/content/mesh"}};
     auto contents = GamebryoModDataContent::getAllContents();
-    contents.push_back(
-        Content(CONTENT_MATERIAL, "Materials", ":/MO/gui/content/material"));
+    std::copy(std::begin(STARFIELD_CONTENTS), std::end(STARFIELD_CONTENTS),
+              std::back_inserter(contents));
     return contents;
   }
 
@@ -32,9 +38,12 @@ public:
   {
     auto contents = GamebryoModDataContent::getContentsFor(fileTree);
     for (auto e : *fileTree) {
-      if (e->compare("materials") == 0) {
-        contents.push_back(CONTENT_MATERIAL);
-        break;  // Early break if you have nothing else to check.
+      if (e->isDir()) {
+        if (e->compare("materials") == 0) {
+          contents.push_back(CONTENT_MATERIAL);
+        } else if (e->compare("geometries") == 0) {
+          contents.push_back(CONTENT_GEOMETRIES);
+        }
       }
     }
     return contents;
