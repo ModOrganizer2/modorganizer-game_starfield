d46f05e668a96554d4898754572f6f237402b8d3
Jeremy Rimpo <jrim@rimpo.org>
2024-06-10 19:12:25 -0500

    Formatting fix

--- src/gamestarfield.h
+++ src/gamestarfield.h
@@ -73,8 +73,8 @@ private:
   bool testFilePresent() const;
 
 private:
-  static const unsigned int PROBLEM_ESP         = 1;
-  static const unsigned int PROBLEM_TEST_FILE   = 2;
+  static const unsigned int PROBLEM_ESP       = 1;
+  static const unsigned int PROBLEM_TEST_FILE = 2;
 
   mutable std::set<QString> m_Active_ESPs;
 };
